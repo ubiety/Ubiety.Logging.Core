5aee2d9afa06568eaaff904dcf1ddcd3e68eb849
Dieter Lunn <coder2000@gmail.com>
2019-12-23 10:05:19 -0600

    docs: add header with license information

--- src/Ubiety.Logging.Core/IUbietyLogManager.cs
+++ src/Ubiety.Logging.Core/IUbietyLogManager.cs
@@ -1,3 +1,8 @@
+/*
+ * Licensed under the MIT license
+ * See the LICENSE file in the project root for more information
+ */
+
 namespace Ubiety.Logging.Core
 {
     /// <summary>
--- src/Ubiety.Logging.Core/UbietyLogger.cs
+++ src/Ubiety.Logging.Core/UbietyLogger.cs
@@ -1,3 +1,8 @@
+/*
+ * Licensed under the MIT license
+ * See the LICENSE file in the project root for more information
+ */
+
 using System;
 using System.Linq;
 
